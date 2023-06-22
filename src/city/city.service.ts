e6856c5163d06c5537d9f4a09fb1ab5180e9416e
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-06-22 16:02:05 -0300

    feat: create city controller and insert cache

---
+++ src/city/city.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { CityEntity } from './entities/city.entity';
+import { Cache } from 'cache-manager';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+
+@Injectable()
+export class CityService {
+  constructor(
+    @InjectRepository(CityEntity)
+    private readonly cityRepository: Repository<CityEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
+
+  async getAllCitieByStateId(stateId: number): Promise<CityEntity[]> {
+    const citiesCache: CityEntity[] = await this.cacheManager.get(
+      `state_${stateId}`,
+    );
+
+    if (citiesCache) {
+      return citiesCache;
+    }
+
+    const cities = await this.cityRepository.find({
+      where: {
+        stateId,
+      },
+    });
+
+    await this.cacheManager.set(`state_${stateId}`, cities);
+
+    return cities;
+  }
+}
